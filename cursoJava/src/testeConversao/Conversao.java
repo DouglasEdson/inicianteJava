827baa7e3df6db60aa20e3d380946c2542de19d5
unknown <douglas22@live.com.pt>
2024-06-24 19:18:39 -0300

    testeVariaveis

---
+++ cursoJava/src/testeConversao/Conversao.java
@@ -0,0 +1,21 @@
+package testeConversao;
+
+public class Conversao {
+
+	public static void main(String[] args) {
+		
+		double salario = 2700.50;
+		int valor = (int) salario;
+		System.out.println(valor);
+		
+		float pontoFlutuante = (float) 3.14;
+		
+		
+		int valor2 = 2000000000;
+		long numeroGrande = 23409893457982374L;
+		short numeroPequeno = 2103;
+		byte b = 127;
+		
+	}
+
+}
