2adc1458b951c8d0f84d6bfbcf6ba1cc6d202406
unknown <douglas22@live.com.pt>
2024-06-25 19:55:33 -0300

    Laço While

---
+++ cursoJava/src/variaveis/TestaWile.java
@@ -0,0 +1,18 @@
+package variaveis;
+
+public class TestaWile {
+
+	public static void main(String[] args) {
+
+		int contador = 0;
+
+		while (contador <= 10) {
+			System.out.println(contador);
+			contador += 1;
+			//contador ++;
+			// contador = contador +1;
+		}
+		System.out.println(contador);
+	}
+
+}
