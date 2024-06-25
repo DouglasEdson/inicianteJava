46f36631cf29373eb5f99961bb9f4c10656b507d
unknown <douglas22@live.com.pt>
2024-06-25 20:29:22 -0300

    Testar For

---
+++ cursoJava/src/variaveis/TestaFor.java
@@ -0,0 +1,11 @@
+package variaveis;
+
+public class TestaFor {
+	public static void main(String[] args) {
+
+		for (int contador = 0; contador <= 10; contador = contador + 1) {
+			System.out.println(contador);
+		}
+	}
+
+}
